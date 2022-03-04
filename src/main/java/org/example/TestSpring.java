461b01ccc460b4d3922d3dd6fd5961592e8ad438
MJSasha <sasha.ushankov@gmail.com>
2022-03-04 13:51:48 +0300

    Task3 (DI)

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -9,8 +9,7 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
         player.playMusic();
         context.close();
     }
